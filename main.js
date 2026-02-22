a56d148972f4cfd48ad5124c4e730df2ae205dd7
Ivan Kurbakov <tayowrld@gmail.com>
2026-02-23 00:34:14 +0300

    removed old console logs from main.js

--- main.js
+++ main.js
@@ -5,9 +5,6 @@ import { initDb, formatVolume, getDb } from "./data/data.js";
 import App from "./ui/app.jsx";
 
 const main = async () => {
-  console.clear();
-  console.log(chalk.blue("Console simulation v.0.1"));
-
   await initDb();
   const { fat } = getDb();
   if (fat.count() === 0) formatVolume({ totalClusters: 256, clusterSize: 64 });
